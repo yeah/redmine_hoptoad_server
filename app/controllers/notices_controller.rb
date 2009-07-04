f122027cdf323c1341e743e39524a9602144a503
root <root@work.yeahrock.de>
2009-07-04 08:01:08 +0000

    * added source links * much nicer issue subjects * custom backtrace filtering

--- app/controllers/notices_controller.rb
+++ app/controllers/notices_controller.rb
@@ -1,39 +1,80 @@
 class NoticesController < ApplicationController
+
   before_filter :check_if_login_required, :except => 'index'
+  before_filter :find_or_create_custom_fields
+
   unloadable  
+
+  TRACE_FILTERS = [
+    /^"On\sline\s#\d+\sof/,
+    /^"\d+:/
+  ]
+
   def index
     notice = YAML.load(request.raw_post)['notice']
     redmine_params = YAML.load(notice['api_key'])
     
     if authorized = Setting.mail_handler_api_key == redmine_params[:api_key]
-      
+
+      # redmine objects
       project = Project.find_by_identifier(redmine_params[:project])
       tracker = project.trackers.find_by_name(redmine_params[:tracker])
       author = User.anonymous
 
-      issue = Issue.find_or_initialize_by_subject_and_project_id_and_tracker_id_and_author_id_and_description(notice['error_message'],
-                                                                                                              project.id,
-                                                                                                              tracker.id,
-                                                                                                              author.id,
-                                                                                                              'Hoptoad Issue')
+      # error class and message
+      error_class = notice['error_class']
+      error_message = notice['error_message']
+
+      # build filtered backtrace
+      backtrace = notice['back'].blank? ? notice['backtrace'] : notice['back']
+      project_trace_filters = (project.custom_value_for(@trace_filter_field).value rescue '').split(/[,\s\n\r]+/)
+      filtered_backtrace = backtrace.reject{|line| (TRACE_FILTERS+project_trace_filters).map{|filter| line.scan(filter)}.flatten.compact.uniq.any?}
+      
+      # build subject by removing method name and '[RAILS_ROOT]', make sure it fits in a varchar
+      subject = "#{error_class} in #{filtered_backtrace.first.split(':in').first.gsub('[RAILS_ROOT]','')}"[0,255]
+      
+      # build description including a link to source repository
+      repo_root = project.custom_value_for(@repository_root_field).value rescue nil
+      repo_file, repo_line = filtered_backtrace.first.split(':in').first.gsub('[RAILS_ROOT]','').split(':')
+      description = "Redmine Notifier reported an Error related to source:#{repo_root}#{repo_file}#L#{repo_line}"
+
+      issue = Issue.find_or_initialize_by_subject_and_project_id_and_tracker_id_and_author_id_and_description(
+        subject,
+        project.id,
+        tracker.id,
+        author.id,
+        description
+      )
                                                                                                               
       if issue.new_record?
+        # set standard redmine issue fields
         issue.category = IssueCategory.find_by_name(redmine_params[:category]) unless redmine_params[:category].blank?
         issue.assigned_to = User.find_by_login(redmine_params[:assigned_to]) unless redmine_params[:assigned_to].blank?
         issue.priority_id = redmine_params[:priority] unless redmine_params[:priority].blank?
+
+        # make sure that custom field error class is associated to this project and tracker
+        project.issue_custom_fields << @error_class_field unless project.issue_custom_fields.include?(@error_class_field)
+        tracker.custom_fields << @error_class_field unless tracker.custom_fields.include?(@error_class_field)
+        
+        # set custom field error class
+        issue.custom_values.build(:custom_field => @error_class_field, :value => error_class)
       end
 
       issue.save!
 
-      journal = issue.init_journal(author, "h4. Backtrace\n\n<pre>#{notice['back'].to_yaml}</pre>\n\n" +
-                                   "h4. Request\n\n<pre>#{notice['request'].to_yaml}</pre>\n\n" +
-                                   "h4. Session\n\n<pre>#{notice['session'].to_yaml}</pre>\n\n" +
-                                   "h4. Environment\n\n<pre>#{notice['environment'].to_yaml}</pre>")
+      journal = issue.init_journal(
+        author, 
+        "h4. Error message\n\n<pre>#{error_message}</pre>\n\n" +
+        "h4. Filtered backtrace\n\n<pre>#{filtered_backtrace.to_yaml}</pre>\n\n" +
+        "h4. Full backtrace\n\n<pre>#{backtrace.to_yaml}</pre>\n\n" +
+        "h4. Request\n\n<pre>#{notice['request'].to_yaml}</pre>\n\n" +
+        "h4. Session\n\n<pre>#{notice['session'].to_yaml}</pre>\n\n" +
+        "h4. Environment\n\n<pre>#{notice['environment'].to_yaml}</pre>"
+      )
 
       if issue.status.blank? or issue.status.is_closed?                                                                                                        
         issue.status = IssueStatus.find(:first, :conditions => {:is_default => true}, :order => 'position ASC')
       end
-
 
       issue.save!
 
@@ -48,5 +89,28 @@ class NoticesController < ApplicationController
       logger.info 'Unauthorized Hoptoad API request.'
       render :status => 403, :text => 'You provided a wrong or no Redmine API key.'
     end
+  end
+  
+  protected
+  
+  def find_or_create_custom_fields
+    @error_class_field = IssueCustomField.find_or_initialize_by_name('Error class')
+    if @error_class_field.new_record?
+      @error_class_field.attributes = {:field_format => 'string', :searchable => true, :is_filter => true}
+      @error_class_field.save(false)
+    end
+
+    @trace_filter_field = ProjectCustomField.find_or_initialize_by_name('Backtrace filter')
+    if @trace_filter_field.new_record?
+      @trace_filter_field.attributes = {:field_format => 'text'}
+      @trace_filter_field.save(false)
+    end
+
+    @repository_root_field = ProjectCustomField.find_or_initialize_by_name('Repository root')
+    if @repository_root_field.new_record?
+      @repository_root_field.attributes = {:field_format => 'string'}
+      @repository_root_field.save(false)
+    end
+
   end
 end
