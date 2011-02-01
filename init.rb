2906831f83c56e835bb5ae7f30a063ca57cf5599
Jan Schulz-Hofen <yeah@yeahrock.de>
2011-02-01 08:21:55 +0100

    updating meta

--- init.rb
+++ init.rb
@@ -2,7 +2,7 @@ require 'redmine'
 
 Redmine::Plugin.register :redmine_hoptoad_server do
   name 'Redmine Hoptoad Server plugin'
-  author 'Jan Schulz-Hofen, ROCKET RENTALS GmbH'
+  author 'Jan Schulz-Hofen, Planio GmbH'
   description 'Turns Redmine into a Hoptoad server, i.e. an API provider which can be used with the hoptoad_notifier. See http://github.com/yeah/redmine_hoptoad_server'
   version '0.0.1'
 end
