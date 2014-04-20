bb854c9c87b9c3530aa5b48f19e11f1c2bf1cb00
Felix Schäfer <felix@fachschaften.org>
2014-04-20 14:55:46 +0200

    Fix for removed IssueObserver (Redmine 2.5) #5

--- lib/redmine_hoptoad_server/patches/issue_patch.rb
+++ lib/redmine_hoptoad_server/patches/issue_patch.rb
@@ -9,6 +9,11 @@ module RedmineHoptoadServer
           def skip_notification?
             @skip_notification == true
           end
+
+          def send_notification_with_skip_notification
+            send_notification_without_skip_notification unless skip_notification?
+          end
+          alias_method_chain :send_notification, :skip_notification
         end
       end
     end
